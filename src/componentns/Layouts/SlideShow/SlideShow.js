2ad201b5b1da5f8d1e0425e90e30fd4f1b56e31a
PetrPozhoga <pet24489@gmail.com>
2020-05-11 19:57:40 +0300

    fix slider arrow prew

--- src/componentns/Layouts/SlideShow/SlideShow.js
+++ src/componentns/Layouts/SlideShow/SlideShow.js
@@ -132,7 +132,7 @@ const SlideShow = ({ children = [], showElements, animationTime = 300, className
           </div>
         </div>
         <div className={ styles.arrowControl }>
-          <svg onClick={ changeSlide.bind(null, 1) } width="50" height="50" viewBox="0 0 50 50" fill="none"
+          <svg onClick={ changeSlide.bind(null, -1) } width="50" height="50" viewBox="0 0 50 50" fill="none"
                xmlns="http://www.w3.org/2000/svg">
             <circle cx="25" cy="25" r="25" fill="#4D3E3D" fillOpacity="0.5"/>
             <path fillRule="evenodd" clipRule="evenodd"
